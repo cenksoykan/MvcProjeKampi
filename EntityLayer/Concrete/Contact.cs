0bd83a32fe5b23c10bd9a7b7f6d39a89b931cfee
cenk <cenk@CONNETIUM-37N24>
2021-08-16 23:12:03 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        [StringLength(50)]
+        public string ContactUserName { get; set; }
+        [StringLength(50)]
+        public string ContactEmailAddress { get; set; }
+        [StringLength(50)]
+        public string ContactSubject { get; set; }
+        [StringLength(2000)]
+        public string ContactMessage { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        [Key]
+        public int WriterId { get; set; }
+        [StringLength(50)]
+        public string WriterName { get; set; }
+        [StringLength(50)]
+        public string WriterSurname { get; set; }
+        [StringLength(500)]
+        public string WriterProfilePicture { get; set; }
+        [StringLength(50)]
+        public string WriterEmailAddress { get; set; }
+        [StringLength(32)]
+        public string WriterPassword { get; set; }
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
+    }
+}
